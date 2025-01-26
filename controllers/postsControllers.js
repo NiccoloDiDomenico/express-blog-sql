39cc74188481b7a578b56f9ddabef92457eac66a
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-01-26 21:44:15 +0100

    feat: complete milestone 4 and finish exercise

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -8,17 +8,32 @@ function index(req, res) {
     const sql = 'SELECT * FROM posts'
 
     // eseguo la query
-    connection.query(sql, (err, result) => {
+    connection.query(sql, (err, results) => {
         if (err) return res.status(500), json({
             error: 'Database query failed'
         })
-        res.json(result);
+        res.json(results);
     })
 };
 
 // show
 function show(req, res) {
+    // recuperiamo l'id dall'URL
+    const id = req.params.id;
 
+    // preparo la query
+    const sql = 'SELECT * FROM posts WHERE id = ?'
+
+    // eseguo la query
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({
+            error: 'Database query failed'
+        });
+        if (results.length === 0) return res.status(404).json({
+            error: 'Pizza not found'
+        });
+        res.json(results[0]);
+    });
 };
 
 // store
